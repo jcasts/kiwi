705431452cbeeb75099e7ab053ed0024abb4890c
jcastagna <jeremie.castagna@gmail.com>
2012-02-21 16:03:22 -0800

    endpoint stuff

--- lib/kiwi/endpoint.rb
+++ lib/kiwi/endpoint.rb
@@ -1,12 +1,20 @@
 ##
 # Describes an application endpoint.
+#
+# Support this:
+#  desc "Get a specific foo"
+#  string :id, "The id of the foo"
+#
+#  get "/foo/:id" do
+#    # do something
+#  end
 
 class Kiwi::Endpoint
 
-  attr_accessor :action, :http_method, :path, :path_name
-                :params, :view, :description
+  attr_reader :http_method, :path, :path_name
+              :params, :view, :description
 
-  def initialize http_method, path
+  def initialize http_method, path, action=nil
     @http_method = http_method.to_s.upcase
     @path_name   = path
     @path        = parse_path path
@@ -15,11 +23,38 @@ class Kiwi::Endpoint
 
     @view        = nil
     @description = nil
-    @params      = {}
+    @params      = {} # Replace with API validator instance
 
     yield self if block_given?
   end
 
+
+  ##
+  # Assign an action proc.
+
+  def action &block
+    @action = block
+  end
+
+
+  ##
+  # Assign a description.
+
+  def desc value
+    @description = value
+  end
+
+
+  ##
+  # Access the param definition object.
+
+  def param
+    @params
+  end
+
+
+  ##
+  # Check that this endpoint supports the request of the given env.
 
   def routes? env
     @http_method == env['HTTP_METHOD'] && env['REQUEST_PATH'] =~ @path
