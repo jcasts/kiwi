4d2655776985d6bf8c0bb6d4dfd34d0a6b9a0a42
Jeremie Castagna <jeremie.castagna@gmail.com>
2012-06-26 14:01:11 -0700

    don't coerce param value if value is correct type or a Validator

--- lib/kiwi/param.rb
+++ lib/kiwi/param.rb
@@ -10,9 +10,11 @@ class Kiwi::Param < Kiwi::Validator::Attribute
 
 
   ##
-  # Coerce a String into the expected param type.
+  # Coerce a String into the expected param type if necessary.
 
   def coerce str
+    return str if Kiwi::Validator === self.type || self.type === str
+
     str  = str.to_s.strip
     rule = Kiwi.input_types[self.type]
     raise TypeError, "Can't coerce #{str.class} into #{self.type}" unless rule
